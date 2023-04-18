ac03f45826c587fe4699db18e3b215dcc3829b37
mcworkaholic <wevans302@gmail.com>
2023-04-18 10:29:31 -0500

    minor changes

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import soundclouddl "github.com/mcworkaholic/go-scdl/cmd/go-scdl"
+import soundclouddl "github.com/mcworkaholic/go-scdl/cmd/scdl"
 
 func main() {
 	// call the cmd Run command to execute the whole program
